637b13d49aed916da78204d433909cdbf8fc8cd8
Andrea Few <almafew@gmail.com>
2021-04-01 04:55:13 -0300

    Ejercicio N° 1. Clase 1. Programa calculador de área y perímetro

---
+++ AREAyPERIMETRO/AREAyPERIMETRO/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AREAyPERIMETRO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int b, a, area, perim, num;
+            string si;
+            do
+            {
+                Console.WriteLine("Elija una figura geométrica");
+                Console.WriteLine("1.Cuadrado");
+                Console.WriteLine("2.Rectángulo");
+                Console.WriteLine("3.Triángulo");
+                num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ingrese la base o el lado");
+                b = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ingrese la altura o el lado");
+                a = Convert.ToInt32(Console.ReadLine());
+                switch (num)
+                {
+
+                    case 1:
+                        area = b * a;
+                        perim = (a * 2) + (b * 2);
+                        Console.WriteLine(" El área del cuadrado es: " + area + " El perímetro del cuadrado es: " + perim);
+                        break;
+                    case 2:
+                        area = b * a;
+                        perim = (a * 2) + (b * 2);
+                        Console.WriteLine(" El área del rectángulo es: " + area + " El perímetro del rectángulo es: " + perim);
+                        break;
+                    case 3:
+                        area = (b * a) / 2;
+                        perim = b * 3;
+                        Console.WriteLine(" El área del triángulo es: " + area + " El perímetro del triángulo es: " + perim);
+                        break;
+
+                }
+                Console.WriteLine("¿Desea volver a intentarlo? Presione 0 para continuar. Enter para salir");
+                si = (Console.ReadLine());
+            }
+            while (si == "0");
+        }
+
+    }
+}
---
+++ AREAyPERIMETRO/AREAyPERIMETRO/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AREAyPERIMETRO")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AREAyPERIMETRO")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c8e9401c-476a-4a3e-8101-16e78a66ff0b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
